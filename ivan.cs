2e795671e1699e1ca824c06de01b975d1962684b
netivan <72153714+netivan@users.noreply.github.com>
2020-10-13 00:07:21 +0200

    Add files via upload

---
+++ ivan.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Klädbutiken
+{
+    class ivn
+    {
+        public static void msgError()
+        {
+            Console.Beep();
+            Console.Write("Wrong!");
+        }
+
+        public static int readNumber(int min, int max)
+        {
+
+            int num = 0;
+            bool error;
+            int origRow = Console.CursorTop;
+            int origCol = Console.CursorLeft;
+
+            do
+            {
+                error = false;
+
+                Console.SetCursorPosition(origCol, origRow);
+                string s = Console.ReadLine();
+
+                try
+                {
+                    num = int.Parse(s);
+                }
+                catch
+                {
+                    error = true;
+                }
+
+                if (num < min || num > max)
+                    error = true;
+
+                if (error == true)
+
+                    msgError();
+
+
+            } while (error);
+
+            return num;
+        }
+
+    }
+}
